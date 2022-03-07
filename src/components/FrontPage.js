7748b78b40f46af0aec317e460dccb3657ca70e3
uch2ha <sinyavskiy.d@gmail.com>
2022-03-07 11:53:23 +0200

    07.03 first commit

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,9 +1,27 @@
-import react from 'react'; 
+import react, { useState } from "react";
 
-function FrontPage(){
-    return(
-        <p>Hello</p>
-    );
+import TemperatureMoreData from "./TemperatureMoreData";
+import PressureMoreData from "./PressureMoreData";
+
+function FrontPage() {
+   const [moreDataIsVisible, setMoreDataIsVisible] = useState("hidden");
+
+   const show_hide_more_data = () => {
+      if (moreDataIsVisible === "hidden") {
+         setMoreDataIsVisible("visible");
+      } else {
+         setMoreDataIsVisible("hidden");
+      }
+   };
+
+   return (
+      <div>
+         <p>Hello</p>
+         <button onClick={() => show_hide_more_data()}>More Data</button>
+         <TemperatureMoreData moreDataIsVisible={moreDataIsVisible} />
+         <PressureMoreData moreDataIsVisible={moreDataIsVisible} />
+      </div>
+   );
 }
 
 export default FrontPage;
