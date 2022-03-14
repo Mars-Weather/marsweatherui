b99a39194914268bd5e96f0c77a7be0d3dce18c9
Irina Tregub <irinatregub@gmail.com>
2022-03-14 21:48:36 +0200

    Combined front-page and more data

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,101 +1,98 @@
-import react, { useState } from "react";
-import styled from "styled-components";
+import react, {useState} from 'react';
+import styled from 'styled-components';
 
 import TemperatureMoreData from "./TemperatureMoreData";
 import PressureMoreData from "./PressureMoreData";
 import WindMoreData from "./WindMoreData";
 
 function FrontPage() {
-   const [moreDataIsVisible, setMoreDataIsVisible] = useState(false);
+    const [moreDataIsVisible, setMoreDataIsVisible] = useState(false);
 
-   const show_hide_more_data = () => {
-      if (moreDataIsVisible) {
-         setMoreDataIsVisible(false);
-      } else {
-         setMoreDataIsVisible(true);
-      }
-   };
+    const show_hide_more_data = () => {
+        if (moreDataIsVisible) {
+            setMoreDataIsVisible(false);
+        } else {
+            setMoreDataIsVisible(true);
+        }
+    };
 
-   return (
-      <Container>
-         <HorizontalDiv>
-            <SolNumberDiv>
-               <p>SOL 259</p>
-            </SolNumberDiv>
-            <TempDiv>
-               <div>
-                  {moreDataIsVisible ? (
-                     <TemperatureMoreData />
-                  ) : (
-                     <p className="general-information">80° F</p>
-                  )}
-               </div>
-            </TempDiv>
-            <PressureDiv>
-               <div>
-                  {moreDataIsVisible ? (
-                     <PressureMoreData />
-                  ) : (
-                     <p className="general-information">761 PA</p>
-                  )}
-               </div>
-            </PressureDiv>
-         </HorizontalDiv>
-         <HorizontalDiv>
-            <div style={{ flex: 8 }}>
-               <p style={{ fontSize: "2rem", color: "white", marginTop: 0 }}>
-                  Season: Summer
-               </p>
-            </div>
-            <ButtonDiv>
-               <button onClick={() => show_hide_more_data()}>More Data</button>
-            </ButtonDiv>
-         </HorizontalDiv>
-         <HorizontalDiv>
-            <WindSircleDiv>
-               <div>
-                  <p style={{ fontSize: "5rem", color: "#CBCBCB" }}>
-                     wind circle
-                  </p>
-               </div>
-            </WindSircleDiv>
-            <WindMoreInfoDiv>
-               <div>{moreDataIsVisible ? <WindMoreData /> : <></>}</div>
-            </WindMoreInfoDiv>
-         </HorizontalDiv>
-      </Container>
-   );
+    return (
+        <Container>
+            <Subcontainer>
+                <LeftColumn>
+                    {moreDataIsVisible ? (
+                    <SolNumberDiv>
+                        <p>SOL 259</p>
+                    </SolNumberDiv>) : (<div></div>)}
+                    <TempDiv>
+                        <div>
+                            {moreDataIsVisible ? (
+                                <TemperatureMoreData/>
+                            ) : (
+                                <p>80° F</p>
+                            )}
+                        </div>
+                    </TempDiv>
+                    <PressureDiv>
+                        <div>
+                            {moreDataIsVisible ? (
+                                <PressureMoreData/>
+                            ) : (
+                                <p>761 PA</p>
+                            )}
+                        </div>
+                    </PressureDiv>
+                </LeftColumn>
+                <CenterColumn>
+                    <p>Season: summer</p>
+                </CenterColumn>
+                <HorizontalDiv>
+                    <WindSircleDiv>
+                        <div>
+                            <p style={{fontSize: "5rem", color: "#CBCBCB"}}>
+                                wind circle
+                            </p>
+                        </div>
+                    </WindSircleDiv>
+                    <WindMoreInfoDiv>
+                        <div>{moreDataIsVisible ? <WindMoreData/> : <></>}</div>
+                    </WindMoreInfoDiv>
+                </HorizontalDiv>
+                <RightColumn>
+                    <Button onClick={() => show_hide_more_data()}>More data</Button>
+                </RightColumn>
+            </Subcontainer>
+
+        </Container>
+    );
 }
 
 export default FrontPage;
 
 const Container = styled.div`
-   min-height: calc(100vh - 70px);
-   position: relative;
-   overflow-x: hidden;
-   padding: 0 calc(3.5vw + 5px);
-   display: flex;
-   flex-direction: row;
-
-   &:before {
-      background: url("/images/mars.jpg") center center / cover no-repeat;
-      position: absolute;
-      content: "";
-      opacity: 0.8;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      z-index: -1;
-   }
+    min-height: calc(100vh - 70px); 
+    position: relative; 
+    overflow-x: hidden; 
+    padding: 0 calc(3.5vw + 5px);
+    
+    &:before{
+        background: url("/images/mars.jpg") center center / cover no-repeat;
+        position: absolute;
+        content:"";
+        opacity: 0.8;
+        top: 0;
+        left: 0; 
+        right: 0;
+        bottom: 0; 
+        z-index: -1; 
+    }
 `;
-
-const HorizontalDiv = styled.div`
-   flex: 1;
-   text-align: center;
-   display: flex;
-   flex-direction: column;
-   padding: 1rem;
+const LeftColumn = styled.div`
+    min-height: 70vh; 
+    width: 30vw; 
+    font-size: 100px; 
+    color: #d9d9d9; 
+    -webkit-text-stroke: 1px #4d4d4d;
 `;
 
 const SolNumberDiv = styled.div`
@@ -109,29 +106,12 @@ const SolNumberDiv = styled.div`
    }
 `;
 
-const ButtonDiv = styled.div`
-   flex: 1;
-   display: flex;
-   justify-content: center;
-   align-items: center;
-
-   button {
-      font-size: 1.5rem;
-   }
-`;
-
 const TempDiv = styled.div`
    flex: 2;
    display: flex;
    justify-content: left;
    align-items: center;
    padding-left: 2rem;
-
-   .general-information {
-      color: #cbcbcb;
-      font-size: 8rem;
-      padding-left: 1rem;
-   }
 `;
 
 const PressureDiv = styled.div`
@@ -140,12 +120,28 @@ const PressureDiv = styled.div`
    justify-content: left;
    align-items: center;
    padding-left: 2rem;
+`;
 
-   .general-information {
-      color: #cbcbcb;
-      font-size: 8rem;
-      padding-left: 1rem;
-   }
+const RightColumn = styled.div`
+    position: fixed; 
+    right: 30px;; 
+    bottom: 30px; 
+`;
+
+const HorizontalDiv = styled.div`
+   flex: 1;
+   text-align: center;
+   display: flex;
+   flex-direction: column;
+   padding: 1rem;
+`;
+
+const CenterColumn = styled.div`
+    width: 30vw;
+    text-align: center;
+    font-size: 45px; 
+    color: #d9d9d9; 
+    -webkit-text-stroke: 1px #4d4d4d;
 `;
 
 const WindSircleDiv = styled.div`
@@ -161,4 +157,23 @@ const WindMoreInfoDiv = styled.div`
    justify-content: right;
    align-items: center;
    padding-right: 2rem;
+`;
+
+const Button = styled.div`
+    height: 40px; 
+    width: 258px; 
+    color: white;
+    display: flex; 
+    justify-content: center; 
+    background-color: rgba(23, 26, 32, 0.8);
+    border-radius: 100px; 
+    opacity:0.65;
+    text-transform: uppercase;
+    align-items: center;
+    cursor: pointer; 
+`;
+
+const Subcontainer = styled.div`
+    padding: 50px; 
+    display: flex; 
 `;
