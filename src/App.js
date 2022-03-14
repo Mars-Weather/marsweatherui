23ff0bcdfa2d49057b4c389d81538850e9c069d3
Irina Tregub <irinatregub@gmail.com>
2022-03-14 23:40:51 +0200

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import react from "react";
 import Header from "./components/Header";
 import FrontPage from "./components/FrontPage";
+import WeekData from "./components/WeekData";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import styled from "styled-components";
 
 function App() {
-   return (
-      <Container>
-         <Header />
-         <FrontPage />
-      </Container>
-   );
+    return (
+        <BrowserRouter>
+            <Container>
+                <Header/>
+                <Routes>
+                    <Route path='/' element={FrontPage()}></Route>
+                    <Route path='/weekdata' element={WeekData()}></Route>
+                </Routes>
+            </Container>
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
 
-const Container = styled.div``;
+const Container = styled.div`
+`;
